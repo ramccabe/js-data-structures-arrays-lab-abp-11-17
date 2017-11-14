1e62ec18eb6c76302f58ef72fbf1bf5afb4c5433
Robert McCabe <robertadammccabe@gmail.com>
2017-11-14 20:00:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 // Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield"];
-  drivers.push = 'John'
+
+function destructivelyAppendDriver(names){
+  return drivers.push('Ralph')
+}
+
+function destructivelyPrependDriver(names){
+  return drivers.unshift('Bob')
+}
+
+function destructivelyRemoveLastDriver(names){
+  return drivers.pop('Garfield')
+}
+
+function destructivelyRemoveFirstDriver(names){
+  return drivers.shift('Milo')
+}
+
+function appendDriver(names){
+  return drivers.concat('Broom')
+}
+
+function prependDriver(arnold){  
+  return [arnold, ...drivers];
+}
+
+function removeLastDriver('Garfield')
